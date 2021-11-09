7de4566fd8b0fb2500718b8a485ff55be8941f07
Denis Renning <denis@devtty.de>
2021-11-09 14:47:29 +0100

    added treeview

--- src/main/java/com/devtty/SampleController.java
+++ src/main/java/com/devtty/SampleController.java
@@ -6,17 +6,16 @@ import java.net.http.*;
 import java.util.*;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.time.Duration;
 import java.util.stream.Stream;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TableColumn;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TreeItemPropertyValueFactory;
 
 
 public class SampleController{
@@ -26,6 +25,8 @@ public class SampleController{
     @FXML private MenuItem fileOpenMenuItem;
 
     @FXML private TableView tableView;
+
+    @FXML private TreeTableView treeTableView;
 	
     List<UrlCheck> urlChecks; 
     
@@ -44,13 +45,80 @@ public class SampleController{
 	    ex.printStackTrace();
 	}
 
+	createTableView();
+	createTreeTableView();
+			   
+	System.out.println("loaded " + urlChecks.size());
+	
+    }
+
+    private void createTableView(){
 	tableView.setItems(FXCollections.observableArrayList(urlChecks));
 	TableColumn<UrlCheck, String> urlCol = new TableColumn<>("Url");
 	urlCol.setCellValueFactory(new PropertyValueFactory<>(urlChecks.get(0).urltocheckProperty().getName()));
 
-	tableView.getColumns().setAll(urlCol);
-			   
-	System.out.println("CHECK FINISHED");
+	TableColumn<UrlCheck, Integer> statusCol = new TableColumn("Status");
+	statusCol.setCellValueFactory(new PropertyValueFactory<>(urlChecks.get(0).statusProperty().getName()));
+
+	tableView.getColumns().setAll(urlCol, statusCol);
+    }
+
+    private void createTreeTableView(){
+
+	UrlCheck rootCheck = new UrlCheck("test");
+	
+	TreeItem<UrlCheck> root = new TreeItem<>(rootCheck);
+	
+	for(UrlCheck uc : urlChecks){
+
+	    try{
+		String host = URI.create(uc.getUrltocheck()).getHost();
+
+		String tld = host.substring(host.lastIndexOf("."));
+
+		String[] h = host.split("\\.");
+		
+		
+		TreeItem<UrlCheck> t = null;
+		for(TreeItem<UrlCheck> t2 : root.getChildren()){
+		    if(t2.getValue().getUrltocheck().equals(h[h.length-1])){
+			t = t2;
+		    }
+		}
+		
+		if(t == null){
+		    t = new TreeItem<>(new UrlCheck(h[h.length-1]));
+		    root.getChildren().add(t);
+		}
+
+		TreeItem<UrlCheck> d = null;
+		for(TreeItem<UrlCheck> t2 : t.getChildren()){
+		    if(t2.getValue().getUrltocheck().equals(h[h.length-2])){
+			d = t2;
+		    }
+		}
+		
+		if(d == null){
+		    d = new TreeItem<>(new UrlCheck(h[h.length-2]));
+		    t.getChildren().add(d);
+		}
+
+	       
+		d.getChildren().add(new TreeItem<>(uc));
+
+	    }catch(Exception u){
+		System.out.println("Import not possible: " + uc.getUrltocheck());
+	    }
+	}
+	treeTableView.setRoot(root);
+	
+	TreeTableColumn<UrlCheck, String> urlCol = new TreeTableColumn<>("Url");
+	TreeTableColumn<UrlCheck, Integer> statusCol = new TreeTableColumn<>("Status");
+
+	treeTableView.getColumns().setAll(urlCol, statusCol);
+
+	urlCol.setCellValueFactory(new TreeItemPropertyValueFactory(rootCheck.urltocheckProperty().getName()));
+	statusCol.setCellValueFactory(new TreeItemPropertyValueFactory(rootCheck.statusProperty().getName()));
     }
 
 
@@ -60,12 +128,13 @@ public class SampleController{
 	//Optional mit direktem (linebyline) check oder hierarchisch optimiert (erst Domain)
 	HttpClient client = HttpClient.newBuilder()
 	    .version(HttpClient.Version.HTTP_2)
-	    .followRedirects(HttpClient.Redirect.NORMAL)
+	    .followRedirects(HttpClient.Redirect.NEVER)
+	    .connectTimeout(Duration.ofSeconds(20))
 	    .build();
 	
-	for(int i=0;i<3;i++){
+	for(int i=0;i<10;i++){
 	    String url = urlChecks.get(i).getUrltocheck();
-	    System.out.println(url);
+
 	    HttpRequest req = HttpRequest.newBuilder()
 		.uri(URI.create(url))
 		.GET()
@@ -74,12 +143,20 @@ public class SampleController{
 	    try{
 		HttpResponse<String> response =
 		    client.send(req, HttpResponse.BodyHandlers.ofString());
-		System.out.println(response.statusCode());
+
+		urlChecks.get(i).setStatus(response.statusCode());
+		
+		if(response.statusCode()!=200){
+		    System.out.println(url);
+		    System.out.println(response.statusCode());
+		}
 	    }catch(Exception e){
 		e.printStackTrace();
 	    }
  	    
 	}
+
+	System.out.println("Http Check finished");
     }
     
     private void process(String s){
--- src/main/java/com/devtty/UrlCheck.java
+++ src/main/java/com/devtty/UrlCheck.java
@@ -5,6 +5,7 @@ import javafx.beans.property.*;
 public class UrlCheck{
 
     private StringProperty urltocheck;
+    private IntegerProperty status;
 
     public void setUrltocheck(String value){
 	urltocheckProperty().set(value);
@@ -12,6 +13,14 @@ public class UrlCheck{
 
     public String getUrltocheck(){
 	return urltocheckProperty().get();
+    }
+    
+    public void setStatus(Integer value){
+	statusProperty().set(value);
+    }
+
+    public Integer getStatus(){
+	return statusProperty().get();
     }
 
     public StringProperty urltocheckProperty(){
@@ -21,6 +30,13 @@ public class UrlCheck{
 	return urltocheck;
     }
 
+    public IntegerProperty statusProperty(){
+	if(status == null)
+	    status = new SimpleIntegerProperty(this, "status", 0);
+	
+	return status;
+    }
+    
     public UrlCheck(String urltocheck){
 	setUrltocheck(urltocheck);
     }
